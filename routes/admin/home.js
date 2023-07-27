822cb4272c906c23658dbf603422edaff3fd5b36
Gonzalo Sanchez <gonzaezeesanchez@gmail.com>
2023-07-27 03:26:21 -0300

    first commit

---
+++ routes/admin/home.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var usersModel = require ('./../../models/usersModel');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('admin/home', 
+  {layout:'admin/layout',
+   persona: req.session.nombre
+});
+});
+module.exports=router;
---
+++ routes/admin/login.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+var usersModel = require ('./../../models/usersModel');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('admin/login', 
+  {layout:'admin/layout'
+});
+});
+
+
+router.get('/logout', function (req,res,next){
+  req.session.destroy();
+  res.render('admin/login',{
+    layout:'admin/layout'
+
+  });
+});
+
+
+router.post ('/', async (req,res,next)=>{
+  try {
+    var usuario =req.body.usuario;
+    var password= req.body.password;
+
+    var data = await usersModel.getUsernameAndPassword(usuario, password);
+
+    if (data != undefined){
+      req.session.id_usuario=data.id;
+      req.session.nombre= data.usuario;
+
+      res.redirect ('/admin/home');
+    } else {
+      res.render ('/admin/login', {
+        layout:'admin/layout',
+        error: true
+      });
+    }
+  }catch(error){console.log(error);
+}})
+
+
+
+module.exports = router;
